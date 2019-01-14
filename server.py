5e3e77eb25f1a0a00c72696d5bee5e0e784dd0a9
SelroyVG <SelroyVG@users.noreply.github.com>
2019-01-14 15:04:34 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,36 +1,30 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import xpathgen as xpath
 
-# HTTPRequestHandler class
 class testHTTPServer_RequestHandler(BaseHTTPRequestHandler):
 
     # GET
     def do_GET(self):
         # Send response status code
         self.send_response(200)
-
         # Send headers
         self.send_header('Content-type', 'application/json')
         self.end_headers()
-
-        # Send message back to client
         message = "Hello world!"
-        # Write content as utf-8 data
         self.wfile.write(bytes(message, "utf8"))
         return
 
     def do_POST(self):
         # Send response status code
         self.send_response(200)
-
         # Send headers
         self.send_header('Content-type', 'application/json')
+        self.send_header('Access-Control-Allow-Origin', '*')
         self.end_headers()
         content_length = int(self.headers['Content-Length'])
         post_data = self.rfile.read(content_length)
-
+        print("Recieved data:", post_data.decode('utf-8'), "\n")
         answer = xpath.exec(post_data.decode('utf-8'))
-
         # Send message back to client
         message = answer
         # Write content as utf-8 data
@@ -39,12 +33,9 @@ class testHTTPServer_RequestHandler(BaseHTTPRequestHandler):
 
 def run():
     print('Starting server...')
-    # Server settings
-    # Choose port 8080, for port 80, which is normally used for a http server, you need root access
-    server_address = ('127.0.0.1', 8081)
+    server_address = ('0.0.0.0', 8081)
     httpd = HTTPServer(server_address, testHTTPServer_RequestHandler)
     print('Running server...')
     httpd.serve_forever()
-
 
 run()
